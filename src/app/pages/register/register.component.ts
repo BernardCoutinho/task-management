a3b416b88b8da14bb15603156c3d3be3a88a47c5
BernardCoutinho <bernard_coutinho2002@hotmail.com>
2024-10-23 17:59:30 -0300

    toasts no registro

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, ReactiveFormsModule, AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';  
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   standalone: true,
@@ -13,13 +14,13 @@ import { Router } from '@angular/router';
 export class RegisterComponent implements OnInit {
   registerForm!: FormGroup;
 
-  constructor(private router: Router, private authService: AuthService) {}
+  constructor(private toastrService: ToastrService, private router: Router, private authService: AuthService) {}
 
   ngOnInit(): void {
     this.initRegisterForm();  
   }
 
-  // Função para criar o FormGroup
+
   private initRegisterForm(): void {
   this.registerForm = new FormGroup({
     email: new FormControl('', [Validators.required]),
@@ -28,8 +29,7 @@ export class RegisterComponent implements OnInit {
   }, { validators: this.passwordsMatchValidator });  
 }
 
-  // Validador customizado para garantir que os campos de senha e confirmação sejam iguais
-  private passwordsMatchValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+   private passwordsMatchValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
   const formGroup = control as FormGroup;
   const password = formGroup.get('password')?.value;
   const confirmPassword = formGroup.get('confirmPassword')?.value;
@@ -48,16 +48,16 @@ register(): void {
     
     this.authService.register(username, email, password).subscribe({
       next: (response) => {
-        console.log('Registro bem-sucedido:', response);
+        this.toastrService.success('Seu registrado foi realizado com sucesso!','Registro bem-sucedido!');
         this.navigateToLogin(); 
       },
       error: (err) => {
         console.error('Erro ao registrar:', err);
-        alert('Ocorreu um erro ao registrar o usuário. Tente novamente.');
+        this.toastrService.error( err.message, "Erro ao registrar!")
       }
     });
   } else {
-    alert('Por favor, preencha todos os campos corretamente.');
+    this.toastrService.warning('Por favor, preencha todos os campos corretamente.', 'Alerta!');
   }
 }
 
